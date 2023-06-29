badda635b3e8f5c150177d239aebb4d8c1f06df5
Matthew Murray <matthewc.murray94@gmail.com>
2023-06-29 14:29:54 -0500

    BookDetails Migration

---
+++ src/db/migrations/20230629185650_bookDetails.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("BookDetails", (table) => {
+        table.increments("id").primary();
+table.decimal('price').notNullable();
+table.decimal('discount').notNullable();
+table.boolean('is_hard_copy');
+table.integer("book_id").unsigned().unique().nullable();
+table.foreign("book_id").references("Book.id").onDelete('CASCADE');
+table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("BookDetails");
+};
